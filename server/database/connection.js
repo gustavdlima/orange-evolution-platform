5e0d9b6d1ec8d047a8171ef765980ab3f57a8630
gstv <gustavdlim@gmail.com>
2022-11-04 12:49:07 -0300

    :bug: erro de conexao ao db corrigido

--- server/database/connection.js
+++ server/database/connection.js
@@ -6,8 +6,6 @@ const connectDB = async () => {
 		const con = mongoose.connect(process.env.MONGO_URI, {
 			useNewUrlParser: true,
 			useUnifiedTopology: true,
-			useFindAndModify: true,
-			useCreateIndex: true
 		})
 
 		console.log('MongoDB connected:${con.connection.host}');

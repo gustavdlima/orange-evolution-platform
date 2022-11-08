b3cacfae123b4ad9e772d9136b67cfd6696d52e4
gstv <gustavdlim@gmail.com>
2022-11-07 22:16:38 -0300

    :recycle: backend refeito
    :fire: Mongodb
    
    :sparkles: conversando com o mysql e recebendo json  do frontend

--- frontend/orange-platform/src/index.js
+++
@@ -1,3 +0,0 @@
-import { createRoot } from 'react-dom/client'
-
-const root = createRoot(document.querySelector("#root"))
---
+++ server/connection.js
@@ -0,0 +1,7 @@
+const mysql = require("mysql");
+
+const db = mysql.createPool({
+	host: "localhost",
+	user: "gstv",
+	password: "123123",
+	data

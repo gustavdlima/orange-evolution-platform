29e8b4aeab4b3d436ab9b2e1c39ec4e5eee8d767
gstv <gustavdlim@gmail.com>
2022-11-11 20:23:45 -0300

    :sparkles: navbar responsiva e funcional

--- client/orange-platform/src/components/navbar/Navbar.jsx
+++ client/orange-platform/src/components/navbar/Navbar.jsx
@@ -1,28 +1,41 @@
-import React from 'react'
-import { useNavigate } from "react-router-dom"
-import { useState } from "react"
-import	Axios from "axios"
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import Axios from "axios";
 
-import './style.css'
+import "./style.css";
 
 const Navbar = () => {
-
-	// const navigate = useNavigate();
+    // const navigate = useNavigate();
     // const routeChange = (path) => {
     //     navigate(path);
     // }
 
-  return (
-  <nav className='nav'>
-				<img className='site-logo' src='./src/assets/logo-branca.png' alt="" href='/' />
-				<div className='list-container'>
-					<ul className='list'>
-						<li><a href=''>Home</a></li>
-						<li><a href=''>Trilhas</a></li>
-						<li><a href=''>Conteúdo</a></li>
-					</ul>
-				</div>
-                <div className="actions">
+    return (
+        <nav className="nav">
+            <div className="row">
+                <div className="col-img">
+                    <img
+                        className="site-logo"
+                        src="./src/assets/logo-branca.png"
+                        alt="orange juice"
+                        href="/"
+                    />
+                </div>
+                <div className="col-6">
+                    <ul className="list">
+                        <li>
+                            <a href="">Home</a>
+                        </li>
+                        <li>
+                            <a href="">Trilhas</a>
+                        </li>
+                        <li>
+                            <a href="">Conteúdo</a>
+                        </li>
+                    </ul>
+                </div>
+                <div className="col">
                     <button type="submit" className="button">
                         Login
                     </button>
@@ -30,8 +43,9 @@ const Navbar = () => {
                         Cadastrar
                     </button>
                 </div>
-	</nav>
+            </div>
+        </nav>
+    );
+};
 
-)}
-
-export default Navbar
+export default Navbar;

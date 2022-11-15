c1b1c43da462cb9cfaceb95ac3fe3b2061dcac55
gstv <gustavdlim@gmail.com>
2022-11-15 17:32:38 -0300

    :sparkles: navbar responsiva adicionada

--- client/orange-platform/src/components/navbar/Navbar.jsx
+++ client/orange-platform/src/components/navbar/Navbar.jsx
@@ -8,71 +8,104 @@ import "./style.css";
 const Navbar = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
-	const [user, setUser] = useState("");
-    const loggedInUser = localStorage.getItem("user");
+    const [user, setUser] = useState("");
+    const isUserLogged = localStorage.getItem("user");
+    const navigate = useNavigate();
 
     const handleLogout = () => {
         setUser([]);
         setEmail("");
         setPassword("");
         localStorage.clear();
+        routeChange("/login");
     };
 
-    const navigate = useNavigate();
     const routeChange = (src) => {
         let path = src;
         navigate(path);
     };
-
-    if (loggedInUser) {
+    if (isUserLogged) {
         return (
             <nav className="nav">
                 <div className="row">
-                    <div className="col-img">
-                        <a href="/">
+
+                    {!isUserLogged.indexOf("admin@mail.com") ? (
+                        <div className="col-6">
+                            <a href="/">
                             <img
                                 className="site-logo"
                                 src="./src/assets/multicolored_logo.png"
                                 alt="orange juice"
                             />
                         </a>
-                    </div>
-                    <div className="col-6">
-                        <ul className="list">
-                            <li>
-                                <a href="/home">Home</a>
-                            </li>
-                            <li>
-                                <a onClick={() => {
-                                     location.href = "https://digital.fcamara.com.br/programadeformacao"
-                                }}>
-                                    Programa de Formação
-                                </a>
-                            </li>
-                            <li>
-                                <a
-                                    onClick={() => {
-                                        routeChange(
-                                            location.href = "https://digital.fcamara.com.br/programadeformacao-momentotbt"
-                                        );
-                                    }}
-                                >
-                                    Cases Hackhaton
-                                </a>
-                            </li>
-                        </ul>
-                    </div>
-                    <div className="col">
-                        <button
-                            type="button"
-                            className="button"
-                            onClick={() => {
-                                handleLogout();
-                            }}
-                        >
-                            Logout
-                        </button>
-                    </div>
+                            <ul className="list">
+                                <li>
+                                    <a href="/home">Admin page</a>
+                                </li>
+                                <li>
+                                    <a>Adicionar Curso</a>
+                                </li>
+                                <li>
+                                    <a>Remover Curso</a>
+                                </li>
+                            </ul>
+                            <button
+                                type="button"
+                                className="button"
+                                onClick={() => {
+                                    handleLogout();
+                                }}
+                            >
+                                Logout
+                            </button>
+                        </div>
+                    ) : (
+                        <div className="col-6">
+                            <a href="/">
+                            <img
+                                className="site-logo"
+                                src="./src/assets/multicolored_logo.png"
+                                alt="orange juice"
+                            />
+                        </a>
+                            <ul className="list">
+                                <li>
+                                    <a href="/home">Home</a>
+                                </li>
+                                <li>
+                                    <a
+                                        onClick={() => {
+                                            location.href =
+                                                "https://digital.fcamara.com.br/programadeformacao";
+                                        }}
+                                    >
+                                        Programa Formação
+                                    </a>
+                                </li>
+                                <li>
+                                    <a
+                                        onClick={() => {
+                                            routeChange(
+                                                (location.href =
+                                                    "https://digital.fcamara.com.br/programadeformacao-momentotbt")
+                                            );
+                                        }}
+                                    >
+                                        Cases Hackhaton
+                                    </a>
+                                </li>
+                            </ul>
+                            <button
+                                type="button"
+                                className="button"
+                                onClick={() => {
+                                    handleLogout();
+                                }}
+                            >
+                                Logout
+                            </button>
+                        </div>
+                    )}
                 </div>
             </nav>
         );
@@ -81,30 +114,45 @@ const Navbar = () => {
     return (
         <nav className="nav">
             <div className="row">
-                <div className="col-img">
+                <div className="col-6">
+                <div className="col">
                     <a href="/">
                         <img
                             className="site-logo"
                             src="./src/assets/multicolored_logo.png"
                             alt="orange juice"
-                        />
+                            />
                     </a>
-                </div>
-                <div className="col-6">
+                    </div>
+                <div className="col">
                     <ul className="list">
                         <li>
                             <a href="/home">Home</a>
                         </li>
                         <li>
-                            <a href="/https://digital.fcamara.com.br/programadeformacao">
-                                Programa de Formação
+                            <a
+                                onClick={() => {
+                                    location.href =
+                                        "https://digital.fcamara.com.br/programadeformacao";
+                                }}
+                            >
+                                Programa Formação
                             </a>
                         </li>
                         <li>
-                            <a href="/conteudo">Cases Hackhaton</a>
+                            <a
+                                onClick={() => {
+                                    routeChange(
+                                        (location.href =
+                                            "https://digital.fcamara.com.br/programadeformacao-momentotbt")
+                                    );
+                                }}
+                            >
+                                Cases Hackhaton
+                            </a>
                         </li>
                     </ul>
-                </div>
+                    </div>
                 <div className="col">
                     <button
                         type="button"
@@ -124,6 +172,7 @@ const Navbar = () => {
                     >
                         Cadastrar
                     </button>
+                </div>
                 </div>
             </div>
         </nav>
--- client/orange-platform/src/components/trilhas/Trilhas.jsx
+++ client/orange-platform/src/components/trilhas/Trilhas.jsx
@@ -1,25 +1,15 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import "./style.css";
+import "./style.scss";
 
 const Trilhas = () => {
     const loggedInUser = localStorage.getItem("user");
-
-    const isUserLogged = () => {
-        const loggedInUser = localStorage.getItem("user");
-        if (loggedInUser) {
-            return 1;
-        } else {
-            return 0;
-        }
-    };
-
     const navigate = useNavigate();
+
     const routeChange = (src) => {
         let path = src;
         navigate(path);
     };
-
     return (
         <div className="trails">
             <div className="row">
@@ -28,7 +18,7 @@ const Trilhas = () => {
                         <li>
                             <a
                                 onClick={() => {
-                                    if (!isUserLogged) {
+                                    if (loggedInUser) {
                                         routeChange("/fullstack");
                                     } else {
                                         routeChange("/cadastro");
@@ -42,7 +32,7 @@ const Trilhas = () => {
                     <div className="col">
                         <li>
                             <a  onClick={() => {
-                                    if (!isUserLogged) {
+                                    if (loggedInUser) {
                                         routeChange("/ux");
                                     } else {
                                         routeChange("/cadastro");
@@ -55,7 +45,7 @@ const Trilhas = () => {
                     <div className="col">
                         <li>
                             <a  onClick={() => {
-                                    if (!isUserLogged) {
+                                    if (loggedInUser) {
                                         routeChange("/qa");
                                     } else {
                                         routeChange("/cadastro");
